a89db7b109b41e1f0d422a570a09603b1dcaa86d
Mateus Pinho <mateusleitepinho@gmail.com>
2022-01-19 18:41:04 +0000

    missing adadahdalsda

--- client.c
+++ client.c
@@ -1,3 +1,33 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+
+#define TAMMSSG 1000
+
+void produzMsg (char *buf) {
+    strcpy (buf, "Mensagem de teste");
+}
+
+void trataMsg (char *buf) {
+    printf("Recebeu: %s\n", buf);
+}
+
+int main () {
+    int fcli, fserv;
+    char buf[TAMMSSG];
+
+    if ((fserv = open("/tmp/servidor", O_WRONLY)) < 0) {
+        return 1;
+    } if ((fcli = open("/tmp/cliente", O_RDONLY)) < 0) {
+        return 1;
+    }
+
+    produzMsg(buf);
+    write(fserv, buf, TAMMSSG);
+    read(fcli, buf, TAMMSSG);
+    trataMsg(buf);
+
+    close(fserv);
+    close(fcli);
+    return 0;
+}
--- server.c
+++ server.c
@@ -1,3 +1,38 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+
+#define TAMMSSG 1000
+
+int main () {
+    int fcli, fserv, n;
+    char buf[TAMMSSG];
+
+    unlink("/tmp/servidor");
+    unlink("/tmp/cliente");
+
+    if (mkfifo("/tmp/servidor", 0777) < 0) {
+        return 1;
+    } if (mkfifo("/tmp/cliente", 0777) < 0) {
+        return 1;
+    }
+
+    if ((fserv = open("/tmp/servidor", O_RDONLY)) < 0) {
+        return 1;
+    } if ((fcli = open("/tmp/cliente", O_WRONLY)) < 0) {
+        return 1;
+    }
+
+    while(1) {
+        n = read(fserv, buf, TAMMSSG);
+        if (n <= 0) {
+            break;
+        }
+        trataPedido(buf);
+        n = write(fcli, buf, TAMMSSG);
+    }
+    close(fserv);
+    close(fcli);
+    unlink("/tmp/servidor");
+    unlink("/tmp/cliente");
+}
